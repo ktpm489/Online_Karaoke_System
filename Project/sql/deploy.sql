1ab1928be3d451fa2cad5a25ad3ac8d6fbc15b0f
Yu Jing <yujing5b5d@gmail.com>
2013-04-27 21:37:34 +0800

    some info

---
+++ Project/sql/deploy.sql
@@ -0,0 +1,59 @@
+﻿/**
+ * deploy db: oks
+ * account usr and password： oksusr , oksusr
+ **/
+create database `oks` default character set utf8 collate utf8_general_ci;
+create user 'oksusr'@'localhost' identified by 'oksusr';
+grant select,insert,update,delete,create,drop,alter,lock tables on `oks`.* to 'oksusr'@'localhost';
+
+use oks;
+
+drop table if exists `List`;
+drop table if exists `Detail`;
+drop table if exists `Music`;
+drop table if exists `User`;
+
+create table `User`
+(
+	UID int primary key auto_increment, 
+	UNAME char(20) unique not null,
+	UPWD char(20) not null,
+	AFLAG tinyint(1) not null default 0
+);
+
+create table `Detail`
+(
+	UID int primary key,
+	DSEX char,
+	DNNAME char(20),
+	DEMAIL char(30),
+	DPN char(20),
+	DLIP char(15),
+	DRIP char(15) not null,
+	foreign key (UID) references User(UID)
+);
+create table `Music`
+(
+	MID int primary key auto_increment,
+	MNAME char(100) not null,
+	MFA char(255) not null,
+	MSA char(255) not null,
+	MART char(50),
+	MAUM char(50),
+	MY int,
+	MC char(50),
+	MT int,
+	MUFLAG tinyint(1) not null default 0,
+	constraint ONE_SONG UNIQUE (MNAME,MAUM)
+);
+
+create table `List`
+(
+    LID int primary key auto_increment,
+    UID int not null,
+	MID int not null,
+	foreign key(UID) references User(UID),
+	foreign key(MID) references Music(MID)
+)
+
+
---
+++ Project/sql/undeploy.sql
@@ -0,0 +1,7 @@
+﻿use mysql;
+revoke all privileges on * . * from 'oksusr'@'localhost';
+revoke all privileges on `oks` . * from 'oksusr'@'localhost';
+revoke grant option on `oks` . * from 'oksusr'@'localhost';
+delete from `user` where convert ( user using utf8) = convert ( 'oksusr' using utf8) and convert (host using utf8) = convert ('localhost' using utf8);
+drop database if exists `oks`;
+
